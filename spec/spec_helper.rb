222c10d3b7421183300ba3ed3a4721b8de4da87b
Francis Hwang <sera@fhwang.net>
2019-03-17 14:20:14 -0400

    Add default rspec rake task.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,17 +3,6 @@ begin
 rescue LoadError
 end
 
-require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  add_filter "spec"
-end
-
 require 'mongoid'
 require 'wisper'
 
